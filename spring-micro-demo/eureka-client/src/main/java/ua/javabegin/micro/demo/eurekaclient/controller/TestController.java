b4cf332805764b07d01ede8130f277364f3ea7d9
yevhen.stadnik <yevhen.stadnik@ihsmarkit.com>
2023-01-15 21:44:26 +0200

    set up default and recommended ports for all applications

--- spring-micro-demo/eureka-client/src/main/java/ua/javabegin/micro/demo/eurekaclient/controller/TestController.java
+++ spring-micro-demo/eureka-client/src/main/java/ua/javabegin/micro/demo/eurekaclient/controller/TestController.java
@@ -1,5 +1,6 @@
 package ua.javabegin.micro.demo.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @GetMapping("/test")
     public String test() {
         return "test";
+    }
+
+    @GetMapping("/id")
+    public String id() {
+        return id;
     }
 
 }
