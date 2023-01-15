d2265f22668fa5ea4392e8326989de56ea7a2d01
yevhen.stadnik <yevhen.stadnik@ihsmarkit.com>
2023-01-15 18:26:43 +0200

    tune eureka client web service application for it to run, register with eureka server and provide some test basic web service features

--- spring-micro-demo/eureka-client/src/main/java/ua/javabegin/micro/demo/eurekaclient/EurekaClientApplication.java
+++ spring-micro-demo/eureka-client/src/main/java/ua/javabegin/micro/demo/eurekaclient/EurekaClientApplication.java
@@ -2,8 +2,11 @@ package ua.javabegin.micro.demo.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+//@EnableDiscoveryClient use this annotation when segregation server is not eureka but something else
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
